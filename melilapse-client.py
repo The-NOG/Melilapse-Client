39b5692ac3e8670cc81d34718ba4fe20d4383a7f
The_NOG <matthew.s.sponaugle@gmail.com>
2023-07-13 21:58:26 -0400

    Begin Validation

--- melilapse-client.py
+++ melilapse-client.py
@@ -1,5 +1,6 @@
 from dotenv import load_dotenv, dotenv_values
 from datetime import datetime
+from astral.geocoder import database, lookup
 import cv2
 
 
@@ -43,16 +44,46 @@ def takePicture():
     print('Taking Picture')
     r, frame = cap.read()
     if r:
-        if bool(config['EnableLocal']):
+        if config['EnableLocal']:
             cv2.imwrite(generateName(),frame)
             #save locally
-        if bool(config['EnableRemote']):
+        if config['EnableRemote']:
             remoteUpload()
     else:
         print('Failed to take picture!')
 
 def validateConfig():
     configValid = True
+    #validate Location
+    try:
+        configCity = lookup(Config["Charleston"], database())
+    except KeyError as e:
+        print("City not recognized")
+        return False
+    #validate Camera Settings
+    try:
+        int(config['CameraID'])
+    except ValueError as identifier:
+        print('CameraID is Invalid')
+        return False
+    #validate Timelapse Settings
+
+    #Validate Local Output settings
+    if config['EnableLocal'] == 'True':
+        config['EnableLocal'] = True
+    elif config['EnableLocal'] == 'False':
+        config['EnableLocal'] = False
+    else:
+        print("EnableLocal is invalid")
+        return False
+    #Validate Remote Output Settings
+    if config['EnableRemote'] == 'True':
+        config['EnableRemote'] = True
+    elif config['EnableRemote'] == 'False':
+        config['EnableRemote'] = False
+    else:
+        print("EnableRemote is invalid")
+        return False
     return configValid
 
 def main():
